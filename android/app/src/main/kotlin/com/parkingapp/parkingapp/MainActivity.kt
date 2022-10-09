713c8ddd67934821a7adde6ee0d8e59f21fdf4cf
safakozdemir <sfkzdmr07@gmail.com>
2022-10-09 19:31:31 +0300

    Template of autoparking project

---
+++ android/app/src/main/kotlin/com/parkingapp/parkingapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.parkingapp.parkingapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static String _title = 'Parking App';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(
+            title: Center(
+          child: Text(
+            _title,
+            style: TextStyle(color: Colors.orange),
+          ),
+        )),
+        body: const Center(
+          child: MyStatefulWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Expanded(
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Expanded(
+            child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Parking Place',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.car_rental, size: 70),
+                      tooltip: 'Reservation Button',
+                      onPressed: () {
+                        setState(() {
+                          print('parking place reservated');
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+          Expanded(
+            child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Parking Place',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.car_rental, size: 70),
+                      tooltip: 'Reservation Button',
+                      onPressed: () {
+                        setState(() {
+                          print('parking place reservated');
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+        ]),
+      ),
+      Expanded(
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Expanded(
+            child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Parking Place',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.car_rental, size: 70),
+                      tooltip: 'Reservation Button',
+                      onPressed: () {
+                        setState(() {
+                          print('parking place reservated');
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+          Expanded(
+            child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Parking Place',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.car_rental, size: 70),
+                      tooltip: 'Reservation Button',
+                      onPressed: () {
+                        setState(() {
+                          print('parking place reservated');
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+        ]),
+      ),
+      Expanded(
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Expanded(
+            child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Parking Place',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.car_rental, size: 70),
+                      tooltip: 'Reservation Button',
+                      onPressed: () {
+                        setState(() {
+                          print('parking place reservated');
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+          Expanded(
+            child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Parking Place',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.car_rental, size: 70),
+                      tooltip: 'Reservation Button',
+                      onPressed: () {
+                        setState(() {
+                          print('parking place reservated');
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+        ]),
+      ),
+      BottomNavigationBar(
+        backgroundColor: Colors.black38,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            label: 'Places',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      )
+    ]);
+  }
+}
