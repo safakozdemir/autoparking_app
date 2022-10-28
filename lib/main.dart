96e084eb2cb6c59ba2dab57e69ef8ff7f5a70240
safakozdemir <sfkzdmr07@gmail.com>
2022-10-28 12:07:17 +0300

    firebase added and app=>gradle minSDK changed to 21

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'places.dart';
 
-void main() => runApp(const MaterialApp(
-      home: MyStatefulWidget(),
-      debugShowCheckedModeBanner: false,
-      title: 'Parking App',
-    ));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MaterialApp(
+    home: MyStatefulWidget(),
+    debugShowCheckedModeBanner: false,
+    title: 'Parking App',
+  ));
+}
 
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({super.key});
